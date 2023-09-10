9c789af92c40f915727a23971f71e7b6ab41f6d7
JeremiSavard <61756597+JeremiSavard@users.noreply.github.com>
2023-09-10 17:42:30 +0000

    Deploying to gh-pages from @ JeremiSavard/jeremi-savard-portfolio@4f68d273079bf84911cf828fba1e296cd3fbc619 🚀

---
+++ _app/immutable/chunks/paths.27852714.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_64cont)==null?void 0:s.base)??"/jeremi-savard-portfolio";var t;const e=((t=globalThis.__sveltekit_64cont)==null?void 0:t.assets)??a;export{e as a,a as b};
